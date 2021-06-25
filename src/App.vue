53be02a10848027c0a82e0325d6a62161e57f6aa
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-06-25 11:04:00 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" height="100" />
+    <h1>Task Manager</h1>
+    <p>Let's build our first Vue.js app!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+@import "./assets/style.scss";
+</style>

43715d1cae1f2e45e2fe2a7697815c99f7be7e10
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-06-25 18:20:36 +0900

    made some update

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,45 +1,87 @@
 <template>
   <div id="tasks">
-    <TaskCard
-      v-for="(task, index) in tasks"
-      :key="index"
-      :title="task.title"
-      :description="task.description"
-      :done="task.done"
-    ></TaskCard>
+    <div v-if="newFormVisible" class="task-card new-task" @keyup.enter="addTask(newTitle, newDescription), resetForm()">
+      <div>
+        <input type="text" placeholder="What would you like to do?" v-model="newTitle" />
+        <textarea placeholder="Add some details about your task..." v-model="newDescription"></textarea>
+      </div>
+    </div>
+    <button class="btn round-icon" @click="newFormVisible = !newFormVisible">
+      +
+    </button>
+    <div v-if="tasks.length > 0">
+      <TaskCard v-for="(task, index) in tasks" :key="index" :title="task.title" :description="task.description" :done="task.done"></TaskCard>
+    </div>
+    <p v-else>You don't have any tasks yet</p>
   </div>
 </template>
-
 <script>
-  import TaskCard from './TaskCard';
+import TaskCard from './TaskCard';
 
-  export default {
-    components: { TaskCard },
+export default {
+  components: { TaskCard },
 
-    data() {
-      return {
-        tasks: [
-          {
-            title: "Create a card component",
-            description:
-              "Create a new TaskCard.vue file in the components folder, then import it in TasksList",
-            done: true,
-          },
-          {
-            title: "Make the card component dynamic",
-            description:
-              "Learn about using the data option and passing data to child components using props",
-            done: true,
-          },
-          {
-            title: "Bind the attributes to the data",
-            description:
-              "Use the v-bind directive to bind the title and description to our data",
-            done: false,
-          },
-        ],
-      };
+  data() {
+    return {
+      tasks: [],
+      newTitle: '',
+      newDescription: '',
+      newFormVisible: false,
+    };
+  },
+
+  methods: {
+    addTask(title, description, done = false) {
+      if (title !== '' && description !== '') {
+        this.tasks.unshift({ title, description, done });
+      }
     },
+
+    resetForm() {
+      this.newTitle = '';
+      this.newDescription = '';
+    }
+  }
+}
+</script>
+<style lang="scss">
+@keyframes expand-vertical {
+  from {
+    min-height: 0;
+    height: 0;
   }
 
-</script>
+  to {
+    min-height: 6rem;
+  }
+}
+
+.task-card.new-task {
+  animation: expand-vertical 0.2s;
+  overflow: hidden;
+  background-color: white;
+  border-left: solid 5px #35495e;
+
+  &,
+  &:hover {
+    transform: scale(1.1);
+    box-shadow: 2px 3px 10px rgba(black, 0.2);
+  }
+
+  &+.tasks-list {
+    pointer-events: none;
+  }
+
+  input {
+    font-size: 1.17rem;
+    font-weight: bold;
+    width: 100%;
+  }
+
+  textarea {
+    width: 100%;
+    font-size: 0.9rem;
+    resize: none;
+  }
+}
+</style>

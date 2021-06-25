1b40d2bde033778a96923d7f471facfa41050cb5
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-06-25 12:40:08 +0900

    props and stuff

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="task-card">
       <div>
-        <h3>Create a card component</h3>
-        <p>Create a new TaskCard.vue file in the components folder, then import it in TasksList.vue</p>
+        <h3>{{ title }}</h3>
+        <p>{{ description }}</p>
       </div>
+      <div>{{ done ? "✅" : "⭕️" }}</div>
     </div>
 </template>
+
+<script>
+  export default {
+    props: {
+      title: String,
+      description: String,
+      done: { type: Boolean, default: false }
+    },
+  }
+</script>
 
 <style lang="scss">
 .task-card {
--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="tasks">
-    <TaskCard></TaskCard>
+    <TaskCard
+      :title="tasks[0].title"
+      :description="tasks[0].description"
+      :done="tasks[0].done"
+    ></TaskCard>
+    <TaskCard
+      :title="tasks[1].title"
+      :description="tasks[1].description"
+      :done="tasks[1].done"
+    ></TaskCard>
+    <TaskCard
+      :title="tasks[2].title"
+      :description="tasks[2].description"
+      :done="tasks[2].done"
+    ></TaskCard>
   </div>
 </template>
 
@@ -9,5 +23,31 @@
 
   export default {
     components: { TaskCard },
+
+    data() {
+      return {
+        tasks: [
+          {
+            title: "Create a card component",
+            description:
+              "Create a new TaskCard.vue file in the components folder, then import it in TasksList",
+            done: true,
+          },
+          {
+            title: "Make the card component dynamic",
+            description:
+              "Learn about using the data option and passing data to child components using props",
+            done: true,
+          },
+          {
+            title: "Bind the attributes to the data",
+            description:
+              "Use the v-bind directive to bind the title and description to our data",
+            done: false,
+          },
+        ],
+      };
+    },
   }
+
 </script>
